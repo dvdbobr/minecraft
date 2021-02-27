739afe02fba85d0b6b50a22e3cec98b59300d902
David Bobritsky <dvdbobr@gmail.com>
2021-02-27 20:27:41 +0200

    fifth commit

--- js/gameWorld.js
+++ js/gameWorld.js
@@ -38,45 +38,53 @@ const blockInventory = {
     "stone": 0,
     "clouds": 0
 }
-const chosenTool = {
-    "shovel": 0,
-    "pickaxe": 1,
-    "axe": 2
-}
-var toolBeingUsed = -1;
+// const chosenTool = {
+//     "shovel": 0,
+//     "pickaxe": 1,
+//     "axe": 2
+// }
+// const chosenBlock = {
+//     "sky": 0,
+//     "dirt": 1,
+//     "grass": 2,
+//     "wood": 3,
+//     "leaves": 4,
+//     "stone": 5,
+//     "clouds": 6
+// }
+let toolBeingUsed = -1;
+let blockBeingUsed = -1;
 //game world
-const gameWorld = document.querySelector('.gameWorld');
+const gameWorld = document.querySelector('#gameWorld');
 const gameLayout = document.querySelector('.gameLayout');
 const menuBtn = document.querySelector('.menu');
 const resetBtn = document.querySelector('.reset');
+const inventoryText = document.querySelectorAll('.inventory span');
+
 //tools
 const shovel = document.querySelector('.gameToolbar>.shovel');
 const pickaxe = document.querySelector('.gameToolbar>.pickaxe');
 const axe = document.querySelector('.gameToolbar>.axe');
 const tools = document.querySelectorAll('.gameToolbar div');
+const blocksInInventory = document.querySelectorAll('.inventory div');
+
+tools.forEach(tool => {
+    tool.addEventListener('click', (e) => {
+        pickTool(e)
+        toolBeingUsed = e.currentTarget.classList[1];
+        console.log(e.currentTarget.classList[1], "is being used now id:", toolBeingUsed)
+    })
+})
+blocksInInventory.forEach(block => {
+    block.addEventListener('click', (e) => {
+        pickBlock(e)
+        blockBeingUsed = e.currentTarget.classList[1];
+        console.log(e.currentTarget.classList[1], "is being used now id:", blockBeingUsed)
+    })
+})
+inventoryText.forEach(block => block.textContent = 0)//initialize count text of each block in inventory
 
 
-// tools.forEach(tool => {
-//     //tool.addEventListener
-//     tool.addEventListener('click', (e) => {
-//         if (e.currentTarget.classList.contains('pickedTool'))
-//             e.currentTarget.classList.remove('pickedTool');
-//         else
-//             e.currentTarget.classList.add('pickedTool');
-//         // toolClass=e.currentTarget.classList[1];
-//         // console.log(e.currentTarget.classList.remove)
-//     })
-// })
-// shovel.addEventListener('click', (e) => {
-//     console.log("click")
-//     shovel.classList.toggle("wbw")
-//     console.log(e.currentTarget)
-// })
-
-// pickTool(shovel)
-// pickTool(pickaxe)
-// pickTool(axe)
-// pickTool(tools)
 function createWorld() {
     for (let row = 0; row < gameMatrix.length; row++) {//create the block in game layout
         for (let col = 0; col < gameMatrix.length; col++) {
@@ -86,63 +94,66 @@ function createWorld() {
             switch (gameMatrix[row][col]) {//assign the proper class and attribute for each block
                 case 1:
                     block.classList.add('dirt');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'dirt');
                     break;
                 case 2:
                     block.classList.add('grass');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'grass');
                     break;
                 case 3:
                     block.classList.add('wood');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'wood');
                     break;
                 case 4:
                     block.classList.add('leaves');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'leaves');
                     break;
                 case 5:
                     block.classList.add('stone');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'stone');
                     break;
                 case 6:
                     block.classList.add('clouds');
+                    block.setAttribute('placeable', 'no')
                     block.setAttribute('block-type', 'clouds');
                     break;
                 default:
                     block.classList.add('sky');
+                    block.setAttribute('placeable', 'yes')
                     block.setAttribute('block-type', 'sky');
                     break;
             }
-            //removeStyleFromAllElements()
             block.addEventListener('click', () => {// increase the count of appropriate blocks in inventory
-                //block.getAttribute('block-type')
-                let countType = "." + block.getAttribute('block-type') + "Count";
-                let countSpan = document.querySelector(countType);
-                console.log(countSpan);
-                countSpan.textContent = ++blockInventory[blockDictionary[gameMatrix[row][col]]];
-                //blockInventory[blockDictionary[gameMatrix[row][col]]]++
-                console.log(block.getAttribute('block-type') + "Count", blockInventory[blockDictionary[gameMatrix[row][col]]]);
+                if (toolBeingUsed == "shovel" && (block.getAttribute('block-type') == 'dirt' || block.getAttribute('block-type') == 'grass'))//shovel
+                    addBlocksToInventory(block, block.getAttribute('block-type'));
+                else if (toolBeingUsed == "pickaxe" && block.getAttribute('block-type') == "stone")//pickaxe
+                    addBlocksToInventory(block, block.getAttribute('block-type'));
+                else if (toolBeingUsed == "axe" && (block.getAttribute('block-type') == 'wood' || block.getAttribute('block-type') == 'leaves'))//axe
+                    addBlocksToInventory(block, block.getAttribute('block-type'));
             });
         }
     }
 }
-//check which tool is picked
-tools.forEach(tool => {
-    tool.addEventListener('click', (e) => {
-        pickTool(e)
-        toolBeingUsed = chosenTool[e.currentTarget.classList[1]];
-        console.log(e.currentTarget.classList[1], "is being used now -id:", toolBeingUsed)
-    })
-})
-//functions
-createWorld()
-
-function resetWorld() {
-    gameLayout.innerHTML = '';
-    createWorld();
+function addBlocksToInventory(block, type) {
+    let countType = "." + block.getAttribute('block-type') + "Count";
+    let countSpan = document.querySelector(countType);
+    console.log(countSpan);
+    countSpan.textContent = ++blockInventory[type];
+    console.log(block.getAttribute('block-type') + "Count", blockInventory[type]);
 }
-//colors background of picked tool
+
+//check which tool is picked
 function pickTool(event) {
+    blockBeingUsed = -1;
+    blocksInInventory.forEach(tool => {
+        if (tool.classList.contains('pickedBlock'))
+            tool.classList.remove('pickedBlock');
+    });
     if (event.currentTarget.classList.contains("pickedTool")) {
         event.currentTarget.classList.remove("pickedTool");
     }
@@ -151,28 +162,96 @@ function pickTool(event) {
         event.currentTarget.classList.add("pickedTool");
     }
 }
-const allBlocks = document.querySelectorAll('.block');
-console.log(allBlocks.length)
-//check mineable blocks
-allBlocks.forEach(block => {
-    block.addEventListener('click', (e) => {
-        if (toolBeingUsed == 0 && (e.currentTarget.getAttribute('block-type')=='dirt' || e.currentTarget.getAttribute('block-type')=='grass')) {//shovel  
-            e.currentTarget.classList.remove('dirt');
-            e.currentTarget.classList.remove('grass');
-            e.currentTarget.classList.add('sky');
-        }
-        if (toolBeingUsed == 1 && e.currentTarget.getAttribute('block-type')=='stone') {//pickaxe
-            e.currentTarget.classList.remove('stone');
-            e.currentTarget.classList.add('sky');
-        }
-        if (toolBeingUsed == 2 && (e.currentTarget.getAttribute('block-type')=='wood' || e.currentTarget.getAttribute('block-type')=='leaves')) {
-            e.currentTarget.classList.remove('wood');
-            e.currentTarget.classList.remove('leaves');
-            e.currentTarget.classList.add('sky');
-        }
-    })
-})
+//check which block is picked
+function pickBlock(event) {
+    toolBeingUsed = -1;
+    tools.forEach(tool => {
+        if (tool.classList.contains('pickedTool'))
+            tool.classList.remove('pickedTool');
+    });
+    if (event.currentTarget.classList.contains("pickedBlock")) {
+        event.currentTarget.classList.remove("pickedBlock");
+    }
+    else {
+        blocksInInventory.forEach((e) => e.classList.remove("pickedBlock"));
+        event.currentTarget.classList.add("pickedBlock");
+    }
+}
+function placeBlockFromInventory() {
 
+}
+
+//check mineable blocks/colors background of picked tool
+function addListenersCheckMineable() {
+
+    const allBlocks = document.querySelectorAll('.block');
+    //console.log(allBlocks.length);
+    allBlocks.forEach(block => {
+        block.addEventListener('click', (e) => {
+            if (toolBeingUsed == "shovel" && (block.getAttribute('block-type') == 'dirt' || block.getAttribute('block-type') == 'grass')) {//shovel  
+                block.classList.remove('dirt', 'grass');
+                block.setAttribute('placeable', 'yes');
+                block.setAttribute('block-type', 'sky');
+                // block.classList.remove('grass');
+                block.classList.add('sky');
+            }
+            if (toolBeingUsed == "pickaxe" && block.getAttribute('block-type') == 'stone') {//pickaxe
+                block.classList.remove('stone');
+                block.setAttribute('placeable', 'yes');
+                block.setAttribute('block-type', 'sky');
+                block.classList.add('sky');
+            }
+            if (toolBeingUsed == "axe" && (block.getAttribute('block-type') == 'wood' || block.getAttribute('block-type') == 'leaves')) {//axe
+                block.classList.remove('wood', 'leaves');
+                block.setAttribute('placeable', 'yes');
+                block.setAttribute('block-type', 'sky');
+                //block.classList.remove('leaves');
+                block.classList.add('sky');
+            }
+        })
+    })
+}
+//check if block can be placed
+function addListenerCheckPlaceable() {
+    const allBlocks = document.querySelectorAll('.block');
+    allBlocks.forEach(block => {
+        block.addEventListener('click', (e) => {
+            console.log("this is the block", blockBeingUsed)
+            checkAndUpdateBlockInventory(block)
+            // if (blockBeingUsed == "dirt" && block.getAttribute('block-type') == 'sky' && blockInventory[blockBeingUsed] > 0) {
+            //     // block.classList.remove('grass');
+            //     block.classList.remove('sky');
+            //     block.classList.add('dirt');
+            //     blockInventory[blockBeingUsed]--;
+            // }
+            // if (blockBeingUsed == "wood" && block.getAttribute('block-type') == 'sky' && blockInventory[blockBeingUsed] > 0) {
+            //     //block.classList.remove('leaves');
+            //     block.classList.remove('sky');
+            //     block.classList.add('wood');
+            // }
+            // if (blockBeingUsed == "stone" && block.getAttribute('block-type') == 'sky' && blockInventory[blockBeingUsed] > 0) {
+            //     block.classList.remove('sky');
+            //     block.classList.add('stone');
+            // }
+        })
+    })
+}
+function checkAndUpdateBlockInventory(block) {
+    if (blockBeingUsed != -1) {
+        let countType = "." + blockBeingUsed + "Count";
+        let countSpan = document.querySelector(countType);
+        console.log("block be used", blockInventory[blockBeingUsed])
+        if (block.getAttribute('placeable') == 'yes' && blockInventory[blockBeingUsed] > 0) {
+            block.classList.remove('sky');
+            block.setAttribute('placeable', 'no')
+            block.setAttribute('block-type', blockBeingUsed)
+            block.classList.add(blockBeingUsed);
+            blockInventory[blockBeingUsed]--;
+            countSpan.textContent = blockInventory[blockBeingUsed];
+            //inventoryText.block.textContent = blockInventory[blockBeingUsed]
+        }
+    }
+}
 function removeStyleFromAllElements() {
     document.querySelector('.dirt').remove(".dirt");
     document.querySelector('.grass').remove(".grass");
@@ -180,14 +259,34 @@ function removeStyleFromAllElements() {
     document.querySelector('.stone').remove(".stone");
     document.querySelector('.clouds').remove(".clouds");
 };
+function resetWorld() {// should reset counter  too
+    for (block in blockInventory) {
+        blockInventory[block] = 0;
+    }
+    inventoryText.forEach(block => block.textContent = 0);
+    tools.forEach(tool => {
+        if (tool.classList.contains('pickedTool')) {
+            tool.classList.remove("pickedTool");
+        }
+    });
+    blocksInInventory.forEach(block => {
+        if (block.classList.contains('pickedBlock')) {
+            block.classList.remove("pickedBlock");
+        }
+    });
+    toolBeingUsed = -1;
+    gameLayout.innerHTML = '';
+    createWorld();
+    addListenersCheckMineable();
+    addListenerCheckPlaceable();
+}
 //go back to menu
 menuBtn.addEventListener('click', () => {
     landingPage.style.display = "block";
-    gameWorld.style.display = "none";
+    gameWorld.style.visibility = "hidden";
+    resetWorld(allBlocks)
 });
 //reset world
-resetBtn.addEventListener('click', () => {
-    resetWorld();
-})
+resetBtn.addEventListener('click', resetWorld)
 
 
--- js/landing-page.js
+++ js/landing-page.js
@@ -3,5 +3,9 @@ const start = document.querySelector('.startGame');
 
 //make landing page disappear
 start.addEventListener('click', () => {
-    landingPage.style.display = "none"
+    landingPage.style.display = "none";
+    gameWorld.style.visibility = "visible";
+    resetWorld();
+    //createWorld();
+    addListenersCheckMineable();
 })
